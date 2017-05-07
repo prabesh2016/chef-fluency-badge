23ce5e5d2444a2dae37866bcf6567e14f51f8594
ramkumar <ursprabesh@gmail.com>
2017-05-06 22:59:23 -0400

    added basic apache cookbook 2

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #install httpd package and start 
 if node ['platform_family'] == "rhel"
-	package = 'httpd'
+	package = "httpd"
 elsif node ['platform_family'] == "debian"
 	package = "apache"
 end
